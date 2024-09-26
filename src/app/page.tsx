334d85f6be6ee62868550b4e23dd307d8a9a0a18
FizzyApple12 <github@fizzyapple12.com>
2024-09-26 19:34:53 -0400

    mobile compatability!

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -53,7 +53,7 @@ const Page: FC = () => {
 
 	return (
 		<div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 md:p-20 font-[family-name:var(--font-geist-sans)]">
-			<main className="flex flex-col gap-0 row-start-2 items-center w-full md:w-1/2">
+			<main className="flex flex-col gap-0 row-start-2 items-center w-full md:w-1/2 min-w-0">
 				<div className="relative w-full">
 					<Binary className="absolute left-3 top-1/2 h-4 w-4 -translate-y-1/2 text-gray-500 transition-all" />
 					<Input
